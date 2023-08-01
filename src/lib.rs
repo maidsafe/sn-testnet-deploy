7639f2ee39434712528f6b331e305394a33eb553
Chris O'Neil <chriso83@protonmail.com>
2023-08-01 15:52:28 +0100

    feat: provide `deploy` command
    The `init` command is subsumed as a new `deploy` command because there's no real reason to have
    these separately; however, all the functionality from the `init` function in the `TestnetDeploy`
    struct is still retained.
    
    At this point, only the Terraform run is being performed. The Ansible run is still to follow.

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ pub mod terraform;
 
 use crate::s3::S3AssetRepository;
 use crate::terraform::{TerraformRunner, TerraformRunnerInterface};
-use color_eyre::Result;
+use color_eyre::{eyre::eyre, Result};
 use flate2::read::GzDecoder;
 use std::fs::File;
 use std::io::BufWriter;
@@ -187,6 +187,33 @@ impl TestnetDeploy {
 
         Ok(())
     }
+
+    pub async fn deploy(
+        &self,
+        name: &str,
+        node_count: u16,
+        repo_owner: Option<String>,
+        branch: Option<String>,
+    ) -> Result<()> {
+        if (repo_owner.is_some() && branch.is_none()) || (branch.is_some() && repo_owner.is_none())
+        {
+            return Err(eyre!(
+                "Both the repository owner and branch name must be supplied if either are used"
+            ));
+        }
+        println!("Will select {name} as workspace...");
+        self.terraform_runner.workspace_select(name)?;
+        let args = vec![
+            ("node_count".to_string(), node_count.to_string()),
+            (
+                "use_custom_bin".to_string(),
+                repo_owner.is_some().to_string(),
+            ),
+        ];
+        println!("Running terraform apply...");
+        self.terraform_runner.apply(args)?;
+        Ok(())
+    }
 }
 
 #[cfg(test)]
@@ -197,7 +224,7 @@ mod test {
     use assert_fs::fixture::ChildPath;
     use assert_fs::prelude::*;
     use assert_fs::TempDir;
-    use color_eyre::Result;
+    use color_eyre::{eyre::eyre, Result};
     use flate2::write::GzEncoder;
     use flate2::Compression;
     use httpmock::prelude::*;
@@ -509,5 +536,159 @@ mod test {
         }
         drop(tmp_dir);
         Ok(())
+    }
+
+    #[tokio::test]
+    async fn deploy_should_run_terraform_apply() -> Result<()> {
+        let (tmp_dir, working_dir) = setup_working_directory()?;
+        let s3_repository = setup_default_s3_repository(&working_dir)?;
+        let mut terraform_runner = MockTerraformRunnerInterface::new();
+        terraform_runner
+            .expect_workspace_select()
+            .times(1)
+            .with(eq("beta".to_string()))
+            .returning(|_| Ok(()));
+        terraform_runner
+            .expect_apply()
+            .times(1)
+            .with(eq(vec![
+                ("node_count".to_string(), "30".to_string()),
+                ("use_custom_bin".to_string(), "false".to_string()),
+            ]))
+            .returning(|_| Ok(()));
+
+        let testnet = TestnetDeploy::new(
+            Box::new(terraform_runner),
+            working_dir.to_path_buf(),
+            CloudProvider::DigitalOcean,
+            s3_repository,
+        );
+
+        testnet.deploy("beta", 30, None, None).await?;
+
+        drop(tmp_dir);
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn deploy_should_run_terraform_apply_with_custom_bin_set_when_repo_is_supplied(
+    ) -> Result<()> {
+        let (tmp_dir, working_dir) = setup_working_directory()?;
+        let s3_repository = setup_default_s3_repository(&working_dir)?;
+        let mut terraform_runner = MockTerraformRunnerInterface::new();
+        terraform_runner
+            .expect_workspace_select()
+            .times(1)
+            .with(eq("beta".to_string()))
+            .returning(|_| Ok(()));
+        terraform_runner
+            .expect_apply()
+            .times(1)
+            .with(eq(vec![
+                ("node_count".to_string(), "30".to_string()),
+                ("use_custom_bin".to_string(), "true".to_string()),
+            ]))
+            .returning(|_| Ok(()));
+
+        let testnet = TestnetDeploy::new(
+            Box::new(terraform_runner),
+            working_dir.to_path_buf(),
+            CloudProvider::DigitalOcean,
+            s3_repository,
+        );
+
+        testnet
+            .deploy(
+                "beta",
+                30,
+                Some("maidsafe".to_string()),
+                Some("custom_branch".to_string()),
+            )
+            .await?;
+
+        drop(tmp_dir);
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn deploy_should_ensure_branch_is_set_if_repo_owner_is_used() -> Result<()> {
+        let (tmp_dir, working_dir) = setup_working_directory()?;
+        let s3_repository = setup_default_s3_repository(&working_dir)?;
+        let mut terraform_runner = MockTerraformRunnerInterface::new();
+        terraform_runner
+            .expect_workspace_select()
+            .times(0)
+            .returning(|_| Ok(()));
+        terraform_runner
+            .expect_apply()
+            .times(0)
+            .returning(|_| Ok(()));
+
+        let testnet = TestnetDeploy::new(
+            Box::new(terraform_runner),
+            working_dir.to_path_buf(),
+            CloudProvider::DigitalOcean,
+            s3_repository,
+        );
+
+        let result = testnet
+            .deploy("beta", 30, Some("maidsafe".to_string()), None)
+            .await;
+
+        match result {
+            Ok(()) => {
+                drop(tmp_dir);
+                Err(eyre!("deploy should have returned an error"))
+            }
+            Err(e) => {
+                assert_eq!(
+                    e.to_string(),
+                    "Both the repository owner and branch name must be supplied if either are used"
+                );
+                drop(tmp_dir);
+                Ok(())
+            }
+        }
+    }
+
+    #[tokio::test]
+    async fn deploy_should_ensure_repo_owner_is_set_if_branch_is_used() -> Result<()> {
+        let (tmp_dir, working_dir) = setup_working_directory()?;
+        let s3_repository = setup_default_s3_repository(&working_dir)?;
+        let mut terraform_runner = MockTerraformRunnerInterface::new();
+        terraform_runner
+            .expect_workspace_select()
+            .times(0)
+            .returning(|_| Ok(()));
+        terraform_runner
+            .expect_apply()
+            .times(0)
+            .returning(|_| Ok(()));
+
+        let testnet = TestnetDeploy::new(
+            Box::new(terraform_runner),
+            working_dir.to_path_buf(),
+            CloudProvider::DigitalOcean,
+            s3_repository,
+        );
+
+        let result = testnet
+            .deploy("beta", 30, None, Some("custom_branch".to_string()))
+            .await;
+
+        match result {
+            Ok(()) => {
+                drop(tmp_dir);
+                Err(eyre!("deploy should have returned an error"))
+            }
+            Err(e) => {
+                assert_eq!(
+                    e.to_string(),
+                    "Both the repository owner and branch name must be supplied if either are used"
+                );
+                drop(tmp_dir);
+                Ok(())
+            }
+        }
     }
 }
