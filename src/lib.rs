2ea310baa5bdbe7f65023a7decae88bb6a387171
Benno Zeeman <bzeeman@live.nl>
2024-01-17 08:38:48 +0100

    refactor: remove comments; small renames

--- src/lib.rs
+++ src/lib.rs
@@ -428,14 +428,6 @@ impl TestnetDeploy {
             )
             .await?;
         let genesis_ip = genesis_inventory[0].1;
-        // let node_info = self
-        //     .rpc_client
-        //     // .get_info(format!("{}:12001", genesis_ip).parse()?, "root", &genesis_ip.to_string())?;
-        //     .get_info(
-        //         (Ipv4Addr::LOCALHOST, 12001).into(),
-        //         "root",
-        //         &genesis_ip.to_string(),
-        //     )?;
 
         let output = self.ssh_client.run_command(
             &genesis_ip,
