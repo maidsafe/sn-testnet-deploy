d87f27419172ac1ccac02b58877c1143446b46ab
Roland Sherwin <RolandSherwin@protonmail.com>
2024-11-12 20:47:40 +0530

    fix: include inactive uploaders when fetching count

--- src/funding.rs
+++ src/funding.rs
@@ -216,7 +216,7 @@ impl AnsibleProvisioner {
                 "Fetching uploader count for {} @ {}",
                 vm.name, vm.public_ip_addr
             );
-            let cmd = "systemctl list-units --type=service | grep autonomi_uploader_ | wc -l";
+            let cmd = "systemctl list-units --type=service --all | grep autonomi_uploader_ | wc -l";
             let result = self
                 .ssh_client
                 .run_command(&vm.public_ip_addr, "root", cmd, true);
