7828a099582faa9a119f6e70a3139ea826e2e8e6
Roland Sherwin <RolandSherwin@protonmail.com>
2024-11-25 12:15:05 +0100

    feat: use attached volumes for node data storage

--- resources/scripts/get_peer_multiaddr.sh
+++ resources/scripts/get_peer_multiaddr.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-rg "is listening on " /var/log/safenode/ | \
+rg "is listening on " /mnt/safenode-storage/log/safenode/ | \
   rg -v "ip4/10." | rg -v "ip4/127." | sort -k1.90,1.119 | head -n +1 | \
   sed -n 's/.*"\(.*\)".*/\1/p'
