32eee86fc858fc6a02671067c1c246237299d00b
Chris O'Neil <chriso83@protonmail.com>
2025-02-03 18:45:59 +0000

    chore: upgrade provider to 2.48.2
    Apparently older versions of the Digital Ocean provider may be what's causing the rate limiting.

--- resources/terraform/testnet/digital-ocean/main.tf
+++ resources/terraform/testnet/digital-ocean/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     digitalocean = {
       source  = "digitalocean/digitalocean"
-      version = "~> 2.0"
+      version = "2.48.2"
     }
   }
   backend "s3" {
