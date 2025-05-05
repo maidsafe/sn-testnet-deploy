527a1d8555e9f53569e604a1e6864fd490e937c4
Chris O'Neil <chriso83@protonmail.com>
2025-05-06 00:08:02 +0100

    chore: allocate reserved ips for alpha network
    As with the prod and staging environments, the `ALPHA-01` network will be designated static IP
    addresses. These can then be used to facilitate easier connections to the network.

--- src/reserved_ip.rs
+++ src/reserved_ip.rs
@@ -4,9 +4,10 @@
 // This SAFE Network Software is licensed under the BSD-3-Clause license.
 // Please see the LICENSE file for more details.
 
-/// These Static IPs are applied to PeerCache VMs in the PROD environment.
-/// If they're already assigned to an environment, re-running the deploy on a new
-/// environment will deallocate them from the previous environment and assign them to the new one.
+/// These static IPs are applied to peer cache VMs.
+///
+/// If they're already assigned to an environment, re-running the deploy on a new environment will
+/// deallocate them from the previous environment and assign them to the new one.
 const PROD_RESERVED_IPS: [&str; 6] = [
     "159.89.251.80",
     "159.65.210.89",
@@ -16,15 +17,15 @@ const PROD_RESERVED_IPS: [&str; 6] = [
     "139.59.198.251",
 ];
 
-const STG_01_RESERVED_IPS: [&str; 3] = [
-    "46.101.64.144",
-    "209.38.170.72",
-    "209.38.170.69",
-    // "209.38.170.58",
-    // "209.38.168.56",
-    // "209.38.160.11",
+const ALPHA_01_RESERVED_IPS: [&str; 5] = [
+    "188.166.133.208",
+    "188.166.133.125",
+    "178.128.137.64",
+    "159.223.242.7",
+    "143.244.197.147",
 ];
 
+const STG_01_RESERVED_IPS: [&str; 3] = ["46.101.64.144", "209.38.170.72", "209.38.170.69"];
 const STG_02_RESERVED_IPS: [&str; 3] = ["188.166.136.212", "157.245.28.60", "138.68.117.227"];
 
 /// Get the reserved IPs for a given environment.
@@ -35,6 +36,9 @@ pub fn get_reserved_ips_args(name: &str) -> Option<String> {
         );
     }
     match name {
+        "ALPHA-01" => Some(
+            serde_json::to_string(&ALPHA_01_RESERVED_IPS).expect("Failed to serialize static IPs"),
+        ),
         "STG-01" => Some(
             serde_json::to_string(&STG_01_RESERVED_IPS).expect("Failed to serialize static IPs"),
         ),
