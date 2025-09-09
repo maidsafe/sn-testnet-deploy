e8dd48d962fc93c18dc070f885e58af8f8ab36e3
Roland Sherwin <RolandSherwin@protonmail.com>
2025-09-09 09:53:14 +0530

    fix: do not error out if no client inventory is found

--- src/deploy.rs
+++ src/deploy.rs
@@ -5,7 +5,10 @@
 // Please see the LICENSE file for more details.
 
 use crate::{
-    ansible::provisioning::{PrivateNodeProvisionInventory, ProvisionOptions},
+    ansible::{
+        inventory::AnsibleInventoryType,
+        provisioning::{PrivateNodeProvisionInventory, ProvisionOptions},
+    },
     error::{Error, Result},
     funding::get_address_from_sk,
     get_anvil_node_data_hardcoded, get_bootstrap_cache_url, get_genesis_multiaddr,
@@ -356,7 +359,8 @@ impl TestnetDeployer {
 
         self.ansible_provisioner
             .print_ansible_run_banner("Provision Uploaders");
-        self.ansible_provisioner
+        let result = self
+            .ansible_provisioner
             .provision_uploaders(
                 &provision_options,
                 genesis_multiaddrs.clone(),
@@ -366,7 +370,12 @@ impl TestnetDeployer {
             .map_err(|err| {
                 println!("Failed to provision Clients {err:?}");
                 err
-            })?;
+            });
+        if let Err(crate::Error::EmptyInventory(AnsibleInventoryType::Clients)) = result {
+            println!("No clients were provisioned as part of this deployment.");
+        } else if let Err(err) = result {
+            return Err(err);
+        }
         self.ansible_provisioner
             .print_ansible_run_banner("Provision Downloaders");
         self.ansible_provisioner
